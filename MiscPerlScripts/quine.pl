f02870ce7997283700fec585e88d5ad253e7d0d8
John R <johnlradford23@gmail.com>
2023-08-08 22:24:39 -0400

    Wrote a perl quine
    * Didn't want to cheat so just tried things till I figured it out.
      - Took me three days.

---
+++ MiscPerlScripts/quine.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/env perl
+# Johns Awesome Perl Quine!
+
+my $source_str = <<'EOF';
+#!/usr/bin/env perl
+# Johns Awesome Perl Quine!
+
+my $source_str = <<'EOF';
+xEOF
+
+my @source = split("\n", $source_str);
+
+for my $line ( @source ) {
+    if ( $line =~ /xEOF/ and $line !~ /\/xEOF\// ) {
+        print $source_str;
+        print "EOF\n";
+        next;
+    }
+
+    print "$line\n";
+}
+EOF
+
+my @source = split("\n", $source_str);
+
+for my $line ( @source ) {
+    if ( $line =~ /xEOF/ and $line !~ /\/xEOF\// ) {
+        print $source_str;
+        print "EOF\n";
+        next;
+    }
+
+    print "$line\n";
+}
