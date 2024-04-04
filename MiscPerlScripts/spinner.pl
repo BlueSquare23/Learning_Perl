b05e34a580453a9eaf3a24af1b8d79158e8d7d83
John R <johnlradford23@gmail.com>
2024-04-04 18:54:01 -0400

    Improve spinner

--- MiscPerlScripts/spinner.pl
+++ MiscPerlScripts/spinner.pl
@@ -7,10 +7,10 @@ use warnings;
 use POSIX ":sys_wait_h";
 use Time::HiRes;
 
-print "Doing some stuff...\n";
+print "Doing the needful...\n";
 
 my $pid;
-
+system("tput civis"); # Hide cursor.
 # Fork the spinner process.
 if (!defined($pid = fork)) {
     die "Cannot fork: $!";
@@ -22,23 +22,25 @@ if (!defined($pid = fork)) {
 
 # Parent process waits for a few seconds, then sends a signal to kill the
 # spinner.
-sleep 3;
+sleep 5;
 kill('TERM', $pid);
 
 # Wait for the spinner to finish.
 waitpid($pid, 0);
+system("tput cnorm"); # Show cursor when done.
 
-print "\rDone!\n";
+print "\rNeedful done!\n";
 print "Doing some other stuff...\n";
 
 sub spinner {
     $|++;
-    my @spin_char = qw(- \ | /);
+#    my @spin_char = qw(- \ | /);
+    my @spin_char = qw(⠈ ⠐ ⠠ ⢀  ⡀ ⠄ ⠂ ⠁);
     my $i = 0;
 
     while (1) {
-        print "\r[$spin_char[$i]]"; 
-        Time::HiRes::sleep(0.3);
+        print "\r$spin_char[$i]"; 
+        Time::HiRes::sleep(0.05);
         if (@spin_char <= ($i + 1)) {
             $i = 0;
         } else {
