75b674f5a9f6c8382a943c06d7a1d829413a7d71
John R <johnlradford23@gmail.com>
2024-04-05 13:51:00 -0400

    Add support for unicode enabled terms
    * Alternativly it'll just print an ascii safe version.

--- MiscPerlScripts/spinner.pl
+++ MiscPerlScripts/spinner.pl
@@ -3,9 +3,12 @@
 
 use strict;
 use warnings;
+use locale;
 
 use POSIX ":sys_wait_h";
 use Time::HiRes;
+
+$|++;
 
 print "Doing the needful...\n";
 
@@ -33,14 +36,30 @@ print "\rNeedful done!\n";
 print "Doing some other stuff...\n";
 
 sub spinner {
-    $|++;
-#    my @spin_char = qw(- \ | /);
-    my @spin_char = qw(⠈ ⠐ ⠠ ⢀  ⡀ ⠄ ⠂ ⠁);
+    my @spin_char;
+    my $t;
+
+    # Check for unicode support.
+    my $lc_all_exists = exists $ENV{'LC_ALL'};
+    my $lc_ctype_exists = exists $ENV{'LC_CTYPE'};
+    my $lang_exists = exists $ENV{'LANG'};
+
+    # Check if the environment variables exist and contain UTF-8.
+    if (($lc_all_exists && $ENV{'LC_ALL'} =~ /UTF-8/i) ||
+        ($lc_ctype_exists && $ENV{'LC_CTYPE'} =~ /UTF-8/i) ||
+        ($lang_exists && $ENV{'LANG'} =~ /UTF-8/i)) {
+        $t = 0.05;
+        @spin_char = qw(⠈ ⠐ ⠠ ⢀  ⡀ ⠄ ⠂ ⠁);
+    } else {
+        $t = 0.25;
+        @spin_char = qw(- \ | /);
+    }
+
     my $i = 0;
 
     while (1) {
         print "\r$spin_char[$i]"; 
-        Time::HiRes::sleep(0.05);
+        Time::HiRes::sleep($t);
         if (@spin_char <= ($i + 1)) {
             $i = 0;
         } else {
