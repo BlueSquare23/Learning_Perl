1b7bdcc86f2821cc0e4a057569385ee8ac6f7702
John R <johnlradford23@gmail.com>
2024-04-11 23:36:44 -0400

    Improve japh

--- MiscPerlScripts/japh.pl
+++ MiscPerlScripts/japh.pl
@@ -6,10 +6,9 @@ use strict;
 
 use MIME::Base64;
 use Compress::Zlib;
-use Data::Dumper;
 
-my $s='blue23';my $p='japh';my @k=split '', crypt($p, $s); my
-%h=('1'=>'CEgt','Y'=>'CNk=','0'=>'UUjM','Q'=>'Ti0C','w'=>'SEzO','l'=>
-'Ki0u','2'=>'SC1S','3'=>'yy/J','U'=>undef,'7'=>undef,'x'=>'ylHw','N'
-=>'AG8p','b'=>'eJzz'); my $japh;foreach (@k){next unless $h{$_};$japh .=
-$h{$_};};print uncompress(decode_base64($japh))."\n";
+my $s='blue23';my $p='japh';my @k=split '', crypt($p, $s);my %h=('1'=>'CEgt',
+'Y'=>'CNk=','0'=>'UUjM','Q'=>'Ti0C','w'=>'SEzO','l'=>'Ki0u','2'=>'SC1S','3'=>
+'yy/J','U'=>undef,'7'=>undef,'x'=>'ylHw','N'=>'AG8p','b'=>'eJzz'); my $japh;
+foreach (@k){next unless $h{$_};$japh.=$h{$_};};print uncompress(decode_base64
+($japh))."\n";
