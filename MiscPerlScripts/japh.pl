9a08b6b179bdf9fff7a2b65c5e3e0e7ad3911072
John R <johnlradford23@gmail.com>
2024-04-11 23:47:12 -0400

    Perl is insane, further improve japh

--- MiscPerlScripts/japh.pl
+++ MiscPerlScripts/japh.pl
@@ -1,8 +1,7 @@
 #!/usr/bin/env perl
 # John's first japh!
-
-use MIME::Base64;use Compress::Zlib;my $s='blue23';my $p='japh';my @k=split '',
-crypt($p, $s);my %h=('1'=>'CEgt','Y'=>'CNk=','0'=>'UUjM','Q'=>'Ti0C','w'=>'SEzO
-','l'=>'Ki0u','2'=>'SC1S','3'=>'yy/J','U'=>undef,'7'=>undef,'x'=>'ylHw','N'=>'A
-G8p','b'=>'eJzz'); my $japh;foreach (@k){next unless $h{$_};$japh.=$h{$_};};
-print uncompress(decode_base64($japh))."\n";
+use MIME::Base64;use Compress::Zlib;$s='blue23';$p='japh';@k=split '',crypt($p,
+$s);%h=('1'=>'CEgt','Y'=>'CNk=','0'=>'UUjM','Q'=>'Ti0C','w'=>'SEzO','l'=>'Ki0u'
+,'2'=>'SC1S','3'=>'yy/J','U'=>undef,'7'=>undef,'x'=>'ylHw','N'=>'AG8p','b'=>'eJ
+zz');$japh;foreach (@k){next unless $h{$_};$japh.=$h{$_};};print uncompress(
+decode_base64($japh))."\n";
