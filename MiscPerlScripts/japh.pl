0b07742e9cb90405337b35c4b80de7da35dd5465
John R <johnlradford23@gmail.com>
2024-04-11 23:07:31 -0400

    Wrote my first ever japh script!

---
+++ MiscPerlScripts/japh.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/env perl
+# John's first japh!
+
+use warnings;
+use strict;
+
+use MIME::Base64;
+use Compress::Zlib;
+use Data::Dumper;
+
+my $s='blue23';my $p='japh';my @k=split '', crypt($p, $s); my
+%h=('1'=>'CEgt','Y'=>'CNk=','0'=>'UUjM','Q'=>'Ti0C','w'=>'SEzO','l'=>
+'Ki0u','2'=>'SC1S','3'=>'yy/J','U'=>undef,'7'=>undef,'x'=>'ylHw','N'
+=>'AG8p','b'=>'eJzz'); my $japh;foreach (@k){next unless $h{$_};$japh .=
+$h{$_};};print uncompress(decode_base64($japh))."\n";
