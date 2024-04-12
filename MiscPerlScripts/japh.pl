55b59fa31d61d46017fd77933d2592cb060ebfe1
John R <johnlradford23@gmail.com>
2024-04-11 23:52:32 -0400

    Optimize japh width for perlmonks
    * For some reason perlmonks wraps lines at 66 chars :shurgy_man:

--- MiscPerlScripts/japh.pl
+++ MiscPerlScripts/japh.pl
@@ -1,7 +1,8 @@
 #!/usr/bin/env perl
 # John's first japh!
-use MIME::Base64;use Compress::Zlib;$s='blue23';$p='japh';@k=split '',crypt($p,
-$s);%h=('1'=>'CEgt','Y'=>'CNk=','0'=>'UUjM','Q'=>'Ti0C','w'=>'SEzO','l'=>'Ki0u'
-,'2'=>'SC1S','3'=>'yy/J','U'=>undef,'7'=>undef,'x'=>'ylHw','N'=>'AG8p','b'=>'eJ
-zz');$japh;foreach (@k){next unless $h{$_};$japh.=$h{$_};};print uncompress(
-decode_base64($japh))."\n";
+use MIME::Base64;use Compress::Zlib;$s='blue23';$p='japh';@k=split 
+'',crypt($p,$s);%h=('1'=>'CEgt','Y'=>'CNk=','0'=>'UUjM','Q'=>'Ti0C
+','w'=>'SEzO','l'=>'Ki0u','2'=>'SC1S','3'=>'yy/J','U'=>undef,'7'=>
+undef,'x'=>'ylHw','N'=>'AG8p','b'=>'eJzz');$japh;foreach (@k){next 
+unless $h{$_};$japh.=$h{$_};};print uncompress(decode_base64($japh
+))."\n";
